07ea99e3e06d1451ad37fdccb16bc630417c9c2c
Dennis HG <dennishg32@gmail.com>
2020-10-04 10:49:54 +0200

    done working on contact us part

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,20 @@ $(document).ready(function() {
         $("#p3").toggle();
         $("#img3").toggle();
     });
+    $("#fills form").submit(function(event) {
+        var name = $("input#name").val();
+        var email = $("input#email").val();
+        var msg = $("textarea#message").val();
+
+        if (name != "" && email != "" && msg != "") {
+            alert(
+                name +
+                ", We have recieved your feedback. Thank you for your support and reach to us, it means a lot"
+            );
+        } else {
+            alert("incomplete, try again please");
+        }
+
+        event.preventDefault();
+    });
 });
